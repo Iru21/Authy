4942cc6ad200c8ab87a4857422bfdb12ebaf6d8f
Iru21 <33987046+Iru21@users.noreply.github.com>
2021-07-05 22:58:30 +0200

    Config creation, add authy command, force login after reload

--- src/main/kotlin/me/mateusz/Authy.kt
+++ src/main/kotlin/me/mateusz/Authy.kt
@@ -1,18 +1,20 @@
 package me.mateusz
 
-import me.mateusz.commands.cLogin
-import me.mateusz.commands.cRegister
-import me.mateusz.commands.cRemember
-import me.mateusz.commands.cUnregister
+import me.mateusz.commands.*
 import me.mateusz.events.LoginEvents
 import me.mateusz.process.LoginProcess
+import me.mateusz.process.Session
 import me.mateusz.utils.CommandFilter
 import me.mateusz.utils.registerCommand
 import org.apache.logging.log4j.core.filter.AbstractFilter
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.block.BlockFace
+import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitTask
 import java.util.logging.Filter
 
 class Authy : JavaPlugin(), Listener {
@@ -22,6 +24,9 @@ class Authy : JavaPlugin(), Listener {
     val CommandFilter : CommandFilter = CommandFilter()
 
     override fun onEnable() {
+
+        saveDefaultConfig()
+
         server.consoleSender.sendMessage("${ChatColor.DARK_GRAY}[${ChatColor.GOLD}$pluginName${ChatColor.DARK_GRAY}] ${ChatColor.GREEN}Enabling $version")
 
         server.pluginManager.registerEvents(LoginEvents(this, LoginProcess), this)
@@ -30,8 +35,46 @@ class Authy : JavaPlugin(), Listener {
         registerCommand(this, cLogin("login", this, LoginProcess))
         registerCommand(this, cUnregister("unregister", this, LoginProcess))
         registerCommand(this, cRemember("remember", this))
+        registerCommand(this, cAuthy("authy", this))
 
         with(CommandFilter) { registerFilter() }
+
+        val Session = Session(this)
+
+        val players = server.onlinePlayers
+        for(player : Player in players) {
+            if(Session.tryAutoLogin(player)) {
+                player.sendMessage("${net.md_5.bungee.api.ChatColor.of("#afffb1")}§l(✔) §7Automatycznie zalogowano!")
+                return
+            }
+
+            lateinit var task0 : BukkitTask
+            var loc = player.location
+            task0 = this.server.scheduler.runTaskTimer(this, Runnable {
+                if(loc.block.getRelative(BlockFace.DOWN).type.isAir) {
+                    loc = Location(loc.world, loc.x, loc.y - 1, loc.z)
+                } else {
+                    task0.cancel()
+                    player.teleport(loc)
+                }
+            }, 0L, 0L)
+
+            LoginProcess.addPlayer(player)
+            var i = 0
+            lateinit var task : BukkitTask
+            task = this.server.scheduler.runTaskTimer(this, Runnable {
+                if(LoginProcess.checkIfContains(player)) {
+                    if(i == 240) {
+                        task.cancel()
+                        player.kickPlayer("§c§l(!) §7Minal czas na autoryzacje!")
+                        LoginProcess.removePlayer(player)
+                    }
+                    LoginProcess.sendPleaseAuthMessage(player)
+                    i++
+                }
+                else task.cancel()
+            },0L, 200L)
+        }
 
     }
 
