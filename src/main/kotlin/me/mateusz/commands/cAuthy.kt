05189e45646c51e2f55f4d0e44f7a7982d898e94
Iru21 <33987046+Iru21@users.noreply.github.com>
2022-02-21 14:48:26 +0100

    fix some permissions

--- src/main/kotlin/me/mateusz/commands/cAuthy.kt
+++ src/main/kotlin/me/mateusz/commands/cAuthy.kt
@@ -19,12 +19,15 @@ class cAuthy(override var name: String = "authy") : ICommand {
                 p.sendMessage("§8[§6Authy§8] §7Created by: §cIru21 §8- §7https://github.com/Iru21/")
                 return true
             } else if(args[0].lowercase() == "reload") {
-                val file = File(authy.dataFolder.absolutePath + "/config.yml")
-                authy.config.load(file)
-                p.sendMessage("§8[§6Authy§8] §7Reloaded §cconfig§7!")
-                translations.updateCache()
-                p.sendMessage("§8[§6Authy§8] §7Reloaded §ctranslations§7!")
-                p.playSound(p.location, Sound.BLOCK_CHAIN_PLACE, 1F, 1F)
+                if(p.hasPermission("authy.reload")) {
+                    val file = File(authy.dataFolder.absolutePath + "/config.yml")
+                    authy.config.load(file)
+                    p.sendMessage("§8[§6Authy§8] §7Reloaded §cconfig§7!")
+                    translations.updateCache()
+                    p.sendMessage("§8[§6Authy§8] §7Reloaded §ctranslations§7!")
+                    p.playSound(p.location, Sound.BLOCK_CHAIN_PLACE, 1F, 1F)
+                }
+
             }
         }
         return true
