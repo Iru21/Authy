780cf69fc81c6ec284f18a05b7bd4f4b5b753d97
Iru21 <33987046+Iru21@users.noreply.github.com>
2022-06-18 13:48:52 +0200

    Move from artifact to shadow build process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,64 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    }
+}
+
 plugins {
+    id("java-library")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("java")
     kotlin("jvm") version "1.6.10"
 }
 
+apply(plugin = "java")
+apply(plugin = "java-library")
+apply(plugin = "com.github.johnrengelman.shadow")
+apply(plugin = "kotlin")
+
 group = "me.iru"
-val mcversion = "1.19"
+val pluginName: String by project
+val pluginVersion: String by project
+val minecraftVersion: String by project
+
+tasks {
+
+    withType<KotlinCompile>() {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    val makeDefaults by registering(Copy::class) {
+        dependsOn(processResources)
+
+        from("src/main/resources/lang/")
+        include("**/*.yml")
+        into("build/resources/main/lang/defaults/")
+    }
+
+    processResources {
+        filesMatching("**plugin.yml") {
+            expand(
+                mutableMapOf(
+                    Pair("pluginVersion", pluginVersion),
+                    Pair("minecraftVersion", minecraftVersion)
+                )
+            )
+        }
+
+    }
+
+    shadowJar {
+        dependsOn(processResources)
+        dependsOn(makeDefaults)
+        archiveFileName.set("${pluginName}-${pluginVersion}-${minecraftVersion}.jar")
+    }
+}
 
 repositories {
     mavenCentral()
@@ -14,16 +67,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.spigotmc:spigot-api:${mcversion}-R0.1-SNAPSHOT")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+
+    compileOnly("org.apache.logging.log4j:log4j-api:2.17.2")
+    compileOnly("org.apache.logging.log4j:log4j-core:2.17.2")
+
+    implementation("org.spigotmc:spigot-api:${minecraftVersion}-R0.1-SNAPSHOT")
     implementation(kotlin("stdlib-jdk8"))
-
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
-}
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
