0db3da2b97a4f77638089bb6722cfb4163817eff
Iru21 <33987046+Iru21@users.noreply.github.com>
2022-06-18 14:16:14 +0200

    Make authy command available to the console

--- src/main/kotlin/me/iru/commands/cAuthy.kt
+++ src/main/kotlin/me/iru/commands/cAuthy.kt
@@ -12,23 +12,23 @@ class cAuthy(override var name: String = "authy") : ICommand {
     val authy = Authy.instance
     val translations = Authy.translations
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if(sender is Player) {
-            val p : Player = sender
-            if(args.size != 1) {
-                p.sendMessage("§8[§6Authy§8] §7Created by: §cIru21 §8- §7https://github.com/Iru21/")
-                return true
-            } else if(args[0].lowercase() == "reload") {
-                if(p.hasPermission("authy.reload")) {
-                    val file = File(authy.dataFolder.absolutePath + "/config.yml")
-                    authy.config.load(file)
-                    p.sendMessage("§8[§6Authy§8] §7Reloaded §cconfig§7!")
-                    translations.updateCache()
-                    p.sendMessage("§8[§6Authy§8] §7Reloaded §ctranslations§7!")
+        if(args.size != 1) {
+            sender.sendMessage("§8[§6Authy§8] §7Created by: §cIru21 §8- §7https://github.com/Iru21/")
+            return true
+        } else if(args[0].lowercase() == "reload") {
+            if(sender.hasPermission("authy.reload")) {
+                val file = File(authy.dataFolder.absolutePath + "/config.yml")
+                authy.config.load(file)
+                sender.sendMessage("§8[§6Authy§8] §7Reloaded §cconfig§7!")
+                translations.updateCache()
+                sender.sendMessage("§8[§6Authy§8] §7Reloaded §ctranslations§7!")
+                if(sender is Player) {
+                    val p: Player = sender
                     p.playSound(p.location, Sound.BLOCK_CHAIN_PLACE, 1F, 1F)
                 }
+            }
 
             }
-        }
         return true
     }
 }
