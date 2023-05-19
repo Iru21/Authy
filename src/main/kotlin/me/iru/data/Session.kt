d6a6b4a9aac0ba59bbec6786be54ebcc2397e75b
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-05-19 17:41:55 +0200

    Remove removed util mention (was useless anyway)

--- src/main/kotlin/me/iru/data/Session.kt
+++ src/main/kotlin/me/iru/data/Session.kt
@@ -2,7 +2,6 @@ package me.iru.data
 
 import me.iru.Authy
 import me.iru.LoginType
-import me.iru.utils.TeleportUtil
 import org.bukkit.entity.Player
 import java.lang.Long.parseLong
 import java.sql.Timestamp
@@ -27,7 +26,6 @@ class Session {
         val curtime = Timestamp(System.currentTimeMillis())
         val timestamp = curtime.time
         if((parseLong(session.toString()) + (authy.config.getInt("sessionExpiresIn") * 3600000) > timestamp) && p.address?.address?.hostAddress == authyPlayer.ip) {
-            TeleportUtil.teleportToValidPlace(p)
             authManager.login(p, LoginType.Session)
             return true
         }
